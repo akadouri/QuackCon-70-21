95c818a35cfa96ea970cac97dc67a07022ede58f
Ariel Kadouri <ariel@arielsartistry.com>
2016-10-15 00:31:10 -0700

    added reactplayer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 var React = require('react');
+var ReactPlayer = require('react-player')
 
 class App extends React.Component {
   render() {
     return (
-      <p> Hello </p>
+      <div>
+        <ReactPlayer url='https://www.youtube.com/watch?v=jIygo3bIVmo' playing />
+      </div>
     );
   }
 }
