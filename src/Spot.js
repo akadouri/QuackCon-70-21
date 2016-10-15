dcb686fb21e5aa69aa20e8c16ea0600245095060
Ckrisirkc <gotaques@yahoo.com>
2016-10-15 06:55:42 -0700

    init add+some work

---
+++ src/Spot.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+var ReactPlayer = require('react-player')
+import { DropTarget } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
+import { ItemTypes } from './Constants';
+
+const spotTarget = { // See: https://gaearon.github.io/react-dnd/docs-drop-target.html
+  canDrop(props) {
+    return true; // TODO: Check if the space is available
+  }
+  // TODO: Make a drop method to swap around modules
+};
+function collect(connect, monitor) { // See: https://gaearon.github.io/react-dnd/docs-drop-target-connector.html
+  return {
+    connectDropTarget: connect.dropTarget()
+  };
+}
+
+class Spot extends React.Component {
+  render() {
+    const { connectDropTarget } = this.props;
+
+    return connectDropTarget(
+      <div>
+        {connectDropTarget} // pls
+      </div>
+    );
+  }
+}
+
+export default DropTarget(ItemTypes.MODULE, spotTarget, collect);
