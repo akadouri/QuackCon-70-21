1e073eb31480a1b35a3dcefc303007235bcf9497
Ariel Kadouri <ariel@arielsartistry.com>
2016-10-15 03:42:19 -0700

    starting plays on field module

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var ReactPlayer = require('react-player')
 var PlayByPlay = require('./PlayByPlay')
+var PlayersOnField = require('./PlayersOnField')
 
 class App extends React.Component {
   render() {
@@ -8,6 +9,7 @@ class App extends React.Component {
       <div>
         <ReactPlayer url='https://www.youtube.com/watch?v=jIygo3bIVmo' playing />
         <PlayByPlay />
+        <PlayersOnField />
       </div>
     );
   }
---
+++ src/PlayersOnField.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+class PlayersOnField extends React.Component {
+  render() {
+    return (
+      <p>A player is on the field</p>
+    );
+  }
+}
+
+module.exports = PlayersOnField;
